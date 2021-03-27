062e0e22c5b3377b8c4c9890d09be24099a53298
Simran Kumari <60690997+simranquirky@users.noreply.github.com>
2021-03-27 22:05:31 +0530

    Create test_code.py

---
+++ test_code.py
@@ -0,0 +1,11 @@
+# This code has an intentional error. You can type it directly or
+# use it for reference to understand the error message below.
+def favorite_ice_cream():
+    ice_creams = [
+        'chocolate',
+        'vanilla',
+        'strawberry'
+    ]
+    print(ice_creams[3])
+
+favorite_ice_cream()
